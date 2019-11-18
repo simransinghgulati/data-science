d5e5542a38128a43664d189e13e47665f94fafaf
Simran <joystickattack@gmail.com>
2019-11-19 02:23:11 +0530

    documentation links added

---
+++ opencv.py
@@ -0,0 +1,20 @@
+# OpenCV for Computer Vision
+# Documentation : https://docs.opencv.org/2.4/
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret, frame = cap.read()
+
+	if ret == False:
+		continue
+
+	cv2.imshow("Stream from the WebCam",frame)
+	key_pressed=cv2.waitKey(0) & 0xFF
+	if key_pressed == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
